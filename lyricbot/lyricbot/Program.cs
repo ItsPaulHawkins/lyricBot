1702a2f96c8849363a41b7c04b99dc52957c0440
Paul Hawkins <hawkinsanthonypaul@gmail.com>
2017-04-29 17:06:44 -0400

    Added project files

---
+++ lyricbot/lyricbot/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lyricbot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Name a song, any song.");
+            string song = Console.ReadLine();
+            string converted = song.Replace(" ", "+");
+            Console.WriteLine(converted);
+            Console.ReadLine();
+        }
+    }
+}
