1daf2311fdfbbe7ed5632b3deb25bc2830c97e39
Paul Hawkins <hawkinsanthonypaul@gmail.com>
2017-05-02 10:35:29 -0400

    Finally prints out lyrics. TODO: Add a loop

--- lyricbot/lyricbot/Program.cs
+++ lyricbot/lyricbot/Program.cs
@@ -4,8 +4,11 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
-using System.Web;   
+using System.Web;
+using HtmlAgilityPack;
+
 
 namespace lyricbot
 {
@@ -16,10 +19,8 @@ namespace lyricbot
             Console.WriteLine("Name a song, any song.");
             string song = Console.ReadLine();
             string converted = song.Replace(" ", "+");
-            Console.WriteLine(converted);
             Console.ReadLine();
             string webdata = new WebClient().DownloadString(new Uri("https://genius.com/search?q=" + converted));
-            Console.WriteLine(webdata);
             int pos = webdata.IndexOf("song_link");
             var lineNumber = webdata.Substring(0, pos).Count(c => c == '\n') + 1;
 
@@ -27,22 +28,24 @@ namespace lyricbot
             var array = webdata.Split('\n');
             string link = array[lineNumber - 1];
             string[] splitLink = link.Split();
-            int i = 0;
-            int ii = 1;
-            string ay = strip(link);
-            Console.WriteLine(ay);
-
-            Console.WriteLine(i);
-            Console.WriteLine(ii);
+            Int32 startIdx = link.IndexOf("https://");
+            Int32 endIdx = link.IndexOf("\"", startIdx);
+            string strippedLink = link.Substring(startIdx, endIdx - startIdx); //strips the link to a regular one. uses the variable link
+            string lyricdata = new WebClient().DownloadString(new Uri(""+ strippedLink));
+            HtmlDocument htmlDoc = new HtmlDocument();
+            htmlDoc.LoadHtml(lyricdata);
+            string result = htmlDoc.DocumentNode.InnerText;
+            result = WebUtility.HtmlDecode(result);
+            Int32 startIndex = result.IndexOf("<!--sse-->");
+            Int32 EndIndex = result.IndexOf("<!--/sse-->");
+            string finalLyrics = result.Substring(startIndex, EndIndex - startIndex);
+            finalLyrics = finalLyrics.Replace("<!--sse-->", " ");
+            finalLyrics = finalLyrics.Replace("â?T", "");
+            Console.WriteLine(finalLyrics);
             Console.ReadLine();
+
+
         }
-        public static string strip(string input)
-        {
-            Int32 startIdx = input.IndexOf("href=\"");
-            if (startIdx < 0) return null;
-            Int32 endIdx = input.IndexOf("\"", startIdx);
-            if (endIdx < 0) return null;
-            return input.Substring(startIdx, endIdx - startIdx);
-        }
+
     }
 }
