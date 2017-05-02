7c097791f207f3fb0a54dace8676968c45ece0c0
Paul Hawkins <hawkinsanthonypaul@gmail.com>
2017-05-02 10:44:03 -0400

    Added a loop function

--- lyricbot/lyricbot/Program.cs
+++ lyricbot/lyricbot/Program.cs
@@ -16,34 +16,43 @@ namespace lyricbot
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Name a song, any song.");
-            string song = Console.ReadLine();
-            string converted = song.Replace(" ", "+");
-            Console.ReadLine();
-            string webdata = new WebClient().DownloadString(new Uri("https://genius.com/search?q=" + converted));
-            int pos = webdata.IndexOf("song_link");
-            var lineNumber = webdata.Substring(0, pos).Count(c => c == '\n') + 1;
-
-            Console.WriteLine(lineNumber);
-            var array = webdata.Split('\n');
-            string link = array[lineNumber - 1];
-            string[] splitLink = link.Split();
-            Int32 startIdx = link.IndexOf("https://");
-            Int32 endIdx = link.IndexOf("\"", startIdx);
-            string strippedLink = link.Substring(startIdx, endIdx - startIdx); //strips the link to a regular one. uses the variable link
-            string lyricdata = new WebClient().DownloadString(new Uri(""+ strippedLink));
-            HtmlDocument htmlDoc = new HtmlDocument();
-            htmlDoc.LoadHtml(lyricdata);
-            string result = htmlDoc.DocumentNode.InnerText;
-            result = WebUtility.HtmlDecode(result);
-            Int32 startIndex = result.IndexOf("<!--sse-->");
-            Int32 EndIndex = result.IndexOf("<!--/sse-->");
-            string finalLyrics = result.Substring(startIndex, EndIndex - startIndex);
-            finalLyrics = finalLyrics.Replace("<!--sse-->", " ");
-            finalLyrics = finalLyrics.Replace("â?T", "");
-            Console.WriteLine(finalLyrics);
-            Console.ReadLine();
-
+            bool loop = true;
+            while (loop == true)
+            {
+                Console.WriteLine("Name a song, any song.");
+                string song = Console.ReadLine();
+                string converted = song.Replace(" ", "+");
+                string webdata = new WebClient().DownloadString(new Uri("https://genius.com/search?q=" + converted));
+                int pos = webdata.IndexOf("song_link");
+                var lineNumber = webdata.Substring(0, pos).Count(c => c == '\n') + 1;
+                var array = webdata.Split('\n');
+                string link = array[lineNumber - 1];
+                string[] splitLink = link.Split();
+                Int32 startIdx = link.IndexOf("https://");
+                Int32 endIdx = link.IndexOf("\"", startIdx);
+                string strippedLink = link.Substring(startIdx, endIdx - startIdx); //strips the link to a regular one. uses the variable link
+                string lyricdata = new WebClient().DownloadString(new Uri("" + strippedLink));
+                HtmlDocument htmlDoc = new HtmlDocument();
+                htmlDoc.LoadHtml(lyricdata);
+                string result = htmlDoc.DocumentNode.InnerText;
+                result = WebUtility.HtmlDecode(result);
+                Int32 startIndex = result.IndexOf("<!--sse-->");
+                Int32 EndIndex = result.IndexOf("<!--/sse-->");
+                string finalLyrics = result.Substring(startIndex, EndIndex - startIndex);
+                finalLyrics = finalLyrics.Replace("<!--sse-->", " ");
+                finalLyrics = finalLyrics.Replace("â?T", "");
+                Console.WriteLine(finalLyrics);
+                Console.WriteLine("Again?");
+                string ifloop = Console.ReadLine();
+                if(ifloop == "yes" || ifloop == "Yes")
+                {
+                    loop = true;
+                }
+                else
+                {
+                    loop = false;
+                }
+            }
 
         }
 
